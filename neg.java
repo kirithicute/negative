630e2d64bd74aa86ef92fdea8428112fb81f9a37
kirithicute <kirithibaskar@gmail.com>
2016-08-30 20:34:24 +0530

    Create neg.java

---
+++ neg.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+public class positive
+{
+public Static void main(String args[])
+{
+int n;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the number");
+n=s.nextInt();
+if (n>0)
+System.out.println("the number is"+n" positive");
+else
+System.out.println("the number is "+n"negative");
+}
+}
